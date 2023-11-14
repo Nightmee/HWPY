88792f0c3168de9b5dab44cda4599bcb08ae79f4
Nightmee <pr1zetyou@gmail.com>
2023-11-14 08:33:35 +0200

    first commit

---
+++ HW9/HW9.py
@@ -0,0 +1,48 @@
+text = 'Hello world'
+a = 0
+while 1==1:
+     print(text[0])
+     print(text[1])
+     print(text[2])
+     print(text[3])
+     print(text[4])
+     print(text[5])
+     print(text[6])
+     print(text[7])
+     print(text[8])
+     print(text[9])
+     print(text[10])
+     break
+#or so
+b = 0
+while b < 10:
+    for i in text:
+        print(i)
+        b += 1
+
+#for i in text:
+     #print(i)
+
+# num1 = 0
+# while 1==1:
+#     print(num1)
+#     num1 += 1
+#     if num1 > 5:
+#         break
+
+# num2 = int(input('Enter number '))
+# stepen = int(input('Enter power '))
+# while 1==1:
+#     print(num2**stepen)
+#     break
+
+# list1 = [i for i in range(0, 16)]
+# print(list1)
+# list1 = [i for i in range(15, -1, -1)]
+# print(list1)
+#
+# spisok = [1, 2, 3, 'hello', True, 3.14]
+# x = 0
+# for i in spisok:
+#     print('%s %s' % (x, i))
+#     x += 1
